3805455e36f0c8867be8ea608b613c37391d951c
yuchenyang <chenyang.yu@hypers.com>
2017-07-14 11:03:49 +0800

    增加user简单的查询方法

--- db/user.go
+++ db/user.go
@@ -1,15 +1,50 @@
 package db
 
 import (
+	"fmt"
 	"time"
 )
 
 type User struct {
 	id         int       `xorm:"int(11) pk not null autoincr"`
-	opnID      string    `xorm:"varhar(100) not null index"` // 小程序内的唯一id
+	openID     string    `xorm:"varhar(100) not null index"` // 小程序内的唯一id
 	nickName   string    `xorm:"varchar(50) not null"`       // 用户昵称
 	avatarURL  string    `xorm:"varchar(50) not null"`       // 用户头像
 	gender     string    `xorm:"varchar(50) not null"`       // 用户性别
 	cretedDate time.Time `xorm:"datetime created"`           // 创建时间
 	updateDate time.Time `xorm:"datetime updated"`           // 上次更新用户信息的时间
+}
+
+func (this *User) GetUserByOpenId(openID string) (*User, bool) {
+	// 根据微信的openId获得用户已经注册的用户信息
+	user := &User{openID: openID}
+	has, err := orm.Get(user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return user, has
+}
+
+func (this *User) Insert() bool {
+	_, err := orm.InsertOne(this)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return true
+}
+
+func (this *User) Delete() bool {
+	_, err := orm.Delete(this)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return true
+}
+
+func (this *User) Update() bool {
+	_, err := orm.Id(this.id).Update(this)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return true
 }
